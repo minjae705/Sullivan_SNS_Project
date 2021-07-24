d3865356c294f69eae025a6e51883eb11565dcfb
minjae <minjae705@gmail.com>
2021-07-24 16:05:26 +0900

    Add: Firebase setting

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import { firestore } from "../firestores/firebase";
 
 export default function Home() {
   return (
